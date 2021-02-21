9a482132c4d5267e00b5a8268c38713839925a5a
manzav95 <zavalamanuel95@gmail.com>
2021-02-21 13:59:42 -0800

    created fully responsive chat app with react using firebase backend

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
 import './App.css';
 
 import firebase from 'firebase/app';
@@ -8,7 +8,7 @@ import 'firebase/auth';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { useCollectionData } from 'react-firebase-hooks/firestore';
 
-firebase.initializeApp({
+const config = {
   apiKey: 'AIzaSyD-HSxkEyYw7RVGGTm7KTZwfev7z3l5Sic',
   authDomain: 'firechat-35474.firebaseapp.com',
   projectId: 'firechat-35474',
@@ -16,7 +16,13 @@ firebase.initializeApp({
   messagingSenderId: '193726300442',
   appId: '1:193726300442:web:4a18067b6e2651441baca3',
   measurementId: 'G-1DPM4MSDQM',
-});
+};
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(config);
+} else {
+  firebase.app();
+}
 
 const auth = firebase.auth();
 const firestore = firebase.firestore();
@@ -40,7 +46,7 @@ function App() {
       auth.signInWithPopup(provider);
     };
 
-    return <button onClick={signInWithGoogle}>Sig In With Google</button>;
+    return <button onClick={signInWithGoogle}>Sign In With Google</button>;
   }
 
   function SignOut() {
@@ -51,22 +57,61 @@ function App() {
     );
   }
   function ChatRoom() {
+    const dummy = useRef();
     const messagesRef = firestore.collection('messages');
     const query = messagesRef.orderBy('createdAt').limit(25);
 
     const [messages] = useCollectionData(query, { idField: 'id' });
+
+    const [formValue, setFormValue] = useState(' ');
+
+    const sendMessage = async (e) => {
+      e.preventDefault();
+
+      const { uid, photoURL } = auth.currentUser;
+
+      await messagesRef.add({
+        text: formValue,
+        createdAt: firebase.firestore.FieldValue.serverTimestamp(),
+        uid,
+        photoURL,
+      });
+
+      setFormValue(' ');
+      dummy.current.scrollIntoView({ behavior: 'smooth' });
+    };
+
     return (
-      <div>
-        {messages &&
-          messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
-      </div>
+      <>
+        <main>
+          {messages &&
+            messages.map((msg) => <ChatMessage key={msg.id} message={msg} />)}
+          <span ref={dummy}></span>
+        </main>
+        <form onSubmit={sendMessage}>
+          <input
+            value={formValue}
+            onChange={(e) => setFormValue(e.target.value)}
+          />
+          <button type='submit' disabled={!formValue}>
+            Submit
+          </button>
+        </form>
+      </>
     );
   }
 
   function ChatMessage(props) {
-    const { text, uid } = props.message;
+    const { text, uid, photoURL } = props.message;
 
-    return <p>{text}</p>;
+    const messageClass = uid === auth.currentUser.uid ? 'sent' : 'recieved';
+
+    return (
+      <div className={`message ${messageClass}`}>
+        <img src={photoURL} alt='' />
+        <p>{text}</p>
+      </div>
+    );
   }
 }
 
